ca0e49fec3c859cc0a64a5ef668407d3f40d5f91
hoshico <melancholinista.yasu@gmail.com>
2021-04-15 00:38:13 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,20 +3,18 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
-    if current_user == @item.user
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user == @item.user
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
-        amount: @item.price,  
-        card: order_params[:token],    
-        currency: 'jpy'                 
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
       )
       @order_address.save
       redirect_to root_path
@@ -28,6 +26,8 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 end

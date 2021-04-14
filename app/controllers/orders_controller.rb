3fc0ef3be0f9c776021c0f2f83fcc44d7f674c31
hoshico <melancholinista.yasu@gmail.com>
2021-04-14 23:49:31 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @oreder_address = OrderAddress.new
+    @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -18,6 +28,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-      params.permit(:token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
-class OrderAddress
+class OrderAddress 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
     
   with_options presence: true do
-    validates :city, :house_number, :user_id, :item_id
+    validates :city, :house_number, :token
     validates :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid."}
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
@@ -11,6 +11,6 @@ class OrderAddress
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end

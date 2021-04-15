93b847fe79f20da06811f0c44dbef3aa66fcc7c2
hoshico <melancholinista.yasu@gmail.com>
2021-04-15 09:57:31 +0900

    修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -48,6 +48,11 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
+      it 'tokenが空だと保存できないこと' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
